f4d057859192af1011b42d88e2114439f966ee02
txu1 <txu1@i9i8.com>
2020-04-15 00:10:25 +0800

    add cookie

--- src/main/java/com/monkey/community/mapper/UserMapper.java
+++ src/main/java/com/monkey/community/mapper/UserMapper.java
@@ -3,12 +3,17 @@ package com.monkey.community.mapper;
 import com.monkey.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author tao
  */
 @Mapper
 public interface UserMapper {
+    @Select("SELECT * FROM user WHERE token = #{token}")
+    User findByToken(@Param("token") String token);
+
     @Insert("INSERT INTO user(name, account_id, token, gmt_create, gmt_modified) values (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
 }
